a77c03428f595b8ec44507f068d204f06989bf02
nikhilsharma9548 <ns630403@gmail.com>
2025-07-11 16:57:11 +0530

    change

--- src/ChatApp/Main.jsx
+++ src/ChatApp/Main.jsx
@@ -22,7 +22,7 @@ const Main = () => {
         animate ={{opacity: 1, x:0}}
         exit={{opacity:0 , x:100}}
         transition={{duration:0.25}}
-        className='h-[460px] w-[330px] rounded-xl bg-gradient-to-t from-white to-cyan-200 shadow-lg'>
+        className='h-[460px] w-[330px] rounded-xl bg-gradient-to-t from-white to-cyan-200 shadow-md shadow-blue-500'>
 
            <div className='flex  justify-between p-5 bg-blue-800/80 rounded-t-xl'>
               <p className='text-3xl text-white '><FaRobot /></p>
@@ -32,6 +32,14 @@ const Main = () => {
             </div>
 
           <div className='h-[70%] overflow-scroll'>
+            {!showResult && (
+              <div className='flex px-7 relative right-5 py-5 gap-2'>
+                <div>
+                  <p className='text-2xl  bg-white rounded-full p-2'><FaRobot /></p>
+                </div>
+                <p className='bg-blue-300 p-2 rounded-md rounded-bl-none'>welcome to Padho Likho! how can i help you</p>
+              </div>
+            )}
           {showResult &&(
              <div className='py-2 flex  flex-col gap-3'>
               <div className='flex  justify-end px-5 pt-2'><p className='bg-blue-300 rounded-lg p-1.5 rounded-br-none  '>{recentPrompt}</p></div>
@@ -47,7 +55,7 @@ const Main = () => {
                     </div>
                   ) : (
                     <div>
-                       <p dangerouslySetInnerHTML={{ __html: resultData || "How can i help you"}}
+                       <p dangerouslySetInnerHTML={{ __html: resultData || "data"}}
                         className='bg-blue-300 p-3 rounded-md rounded-bl-none'></p>
                     </div>
                   )}
--- src/ChatApp/gemini.js
+++ src/ChatApp/gemini.js
@@ -11,7 +11,7 @@ const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-
         role: "user",
         parts: [{  text: `
           if the is about the courses then answer "our courses details is the below the courses section click and read anythink about my courses "
-          " your owner is "nikhil sharma" 
+          " my owner is "nikhil sharma" 
           "hello there ! Welcome to Padho Likho ! How can i help you"
           "you are only answered my LMS based questions "
         You are an assistant for an LMS (Learning Management System). Only answer questions that are related to LMS such as assignments, courses, students, results, login issues, dashboard, etc. 
