91de2462d3a3c8cefc0532e6497f0619eb860fdb
nikhilsharma9548 <ns630403@gmail.com>
2025-08-10 17:55:21 +0530

    push

--- Server/Routes/authRouter.js
+++ Server/Routes/authRouter.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { isAuthenticated, resetPassword, sendResetOTP, userLogin, userLogout, userRegister } from '../Controllers/authController.js';
+import userAuth from '../MiddleWare/userAuth.js'
 
 
 const authRouter = express.Router();
@@ -7,7 +8,7 @@ const authRouter = express.Router();
 authRouter.post('/register',userRegister)
 authRouter.post('/login',userLogin)
 authRouter.post('/logout',userLogout)
-authRouter.get('/is-auth',isAuthenticated)
+authRouter.get('/is-auth',userAuth, isAuthenticated)
 authRouter.post('/send-reset-otp',sendResetOTP)
 authRouter.post('/reset-password',resetPassword)
 
