8c00a08602ead64ca88afb361d1c2c10f1ee213a
nikhilsharma9548 <ns630403@gmail.com>
2025-08-16 19:45:14 +0530

    almost

--- Server/server.js
+++ Server/server.js
@@ -11,9 +11,10 @@ connectDB();
 const app = express()
 const PORT = process.env.PORT || 8000
 
-const allowAll =["http://localhost:5173"]
+const allowAll =[" https://learn-management-system-frontend.vercel.app"]
 
-// https://learn-management-system-frontend.vercel.app
+//
+
 
 app.use(express.json());
 app.use(cookieParser());
